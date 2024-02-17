c90fa2921862475a2cae068c6970044d939446d4
muhammed <muhammedsuleey@gmail.com>
2024-02-17 15:34:52 +0300

    ekleposta

--- eklentili eposta.py
+++ eklentili eposta.py
@@ -50,3 +50,36 @@ host = "smtp.gmail.com"
 eposta_sunucu = smtplib.SMTP_SSL(host=host, port=port, context=context)
 eposta_sunucu.login(kullanici, sifre)
 eposta_sunucu.sendmail(kullanici, alici, posta_str)
+
+from imap_tools import MailBox
+posta_kutusu = MailBox('imap.gmail.com')
+posta_kutusu.login(kullanici, sifre, initial_folder="INBOX")
+import datetime
+from imap_tools import AND
+kriter = AND(date_gte=datetime.date(2024,1,30), from_=kullanici)
+for msg in posta_kutusu.fetch(kriter):
+    print(msg.text)
+
+with  MailBox('imap.gmail.com').login(kullanici, sifre, initial_folder="INBOX") as posta_kutusu:
+        kriter = AND(date_gte=datetime.date(2024, 1, 30), from_=kullanici)
+        for msg in posta_kutusu.fetch(kriter):
+            print(msg.text)
+
+
+dosya_ismi = 'e-posta.ipynb'
+def dosya_isminden_mail_bul(eposta_kutusu_param, dosya_ismi_param, kriter_param):
+    for mesaj in eposta_kutusu_param.fetch(kriter):
+        if mesaj.attachments:
+            for ek in mesaj.attachments:
+                if dosya_ismi_param == ek.filename:
+                    return('{} isimli dosya, {} tarihli ve {} baslikli epostadir.'.format(
+                        dosya_ismi_param,
+                        mesaj.date_str,
+                        mesaj.subject
+                    ))
+    return ('{} isimli dosya, eposta kutusunda bulunamadi.'.format(dosya_ismi_param))
+
+
+
+
+
